c7e813692f8d4f7a0d44363c3d1ea32154347a30
wafa fathima <wafathima15@gmail.com>
2026-05-17 10:18:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//   tailwindcss(),
+//   ],
+//   server:{
+//     port:4000
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
-  ],
-  server:{
-    port:4000
+  plugins: [react()
+    tailwindcss(),       
+           ],
+  server: {
+    port: 4000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5050',
+        changeOrigin: true,
+      }
+    }
+  },
+  // Make sure environment variables are properly loaded
+  define: {
+    'process.env': {}
   }
 })
