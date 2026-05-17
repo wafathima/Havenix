e38a831562b90b7377461651fa1515c8b7cf76bd
wafa fathima <wafathima15@gmail.com>
2026-05-17 12:28:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5050',
+        target: 'https://havenix-backend.onrender.com/',
         changeOrigin: true,
       }
     }
